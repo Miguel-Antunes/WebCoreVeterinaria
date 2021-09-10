0f880d00f29aa72c993409733c6d4fd8bef86b36
Miguel-Antunes <83986406+Miguel-Antunes@users.noreply.github.com>
2021-09-09 22:29:34 -0300

    criação das classes vacina e controleVacina

---
+++ Models/Dominio/Vacina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinaria.Models.Dominio
+{
+    public class Vacina
+    {
+        public int idVacina { get; set; }
+        public string descricaoVacina { get; set; }
+        public string fabricanteVacina { get; set; }
+        public string fabricacaoVacina { get; set; }
+        public string validadeVacina { get; set; }
+
+
+    }
+}
