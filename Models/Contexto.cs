0cd8fbdeab1a1a02a404a0410a8b36c4af07d32f
Miguel-Antunes <83986406+Miguel-Antunes@users.noreply.github.com>
2021-09-12 16:19:50 -0300

    Criação do contexto

---
+++ Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using ClinicaVeterinaria.Models.Dominio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinaria.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Veterinario> Veterinarios { get; set; }
+        public DbSet<Vacina> Vacinas { get; set; }
+        public DbSet<Procedimento> Procedimentos { get; set; }
+        public DbSet<Animal> Animais { get; set; }
+    }
+}
